04472abf10536cd97bb1534f6cd36ea478dfe3e8
hy911814 <hy911814@outlook.com>
2025-02-25 12:55:05 +0800

    2025.2.25

--- HardWares/通讯模块/usb通讯模块/板卡/DAQmxCounterOutChannel/PulseBlaster.cs
+++ HardWares/通讯模块/usb通讯模块/板卡/DAQmxCounterOutChannel/PulseBlaster.cs
@@ -66,7 +66,7 @@ namespace HardWares.板卡.DAQmxCounterSignalChannel
 
         bool USBInternalInterface.TestUSBAction()
         {
-            ChannelInds = new List<int>().AsEnumerable();
+            ChannelInds = new List<int>() { 0 }.AsEnumerable();
             return true;
         }
 
